4eab1ff3fc6e12275d4373ffc4cdf68682e3018b
SachinHanuvel <sachin@hanuvel.com>
2023-05-22 13:43:15 +0530

    created portfolio using Angular still there is lot of work to do

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ScrollToService } from '@nicky-lenaers/ngx-scroll-to';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent {
+  // constructor(private scrollToService: ScrollToService) { }
+
+  // scrollTo(sectionId: string): void {
+  //   this.scrollToService.scrollTo({
+  //     target: sectionId,
+  //     duration: 500, // Adjust the duration as needed
+  //     easing: 'easeInOutQuart' // Adjust the easing function as desired
+  //   });
+  // }
+}
